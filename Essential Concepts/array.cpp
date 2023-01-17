8855d079c11a752b5d011e86b8f64fb20b670b97
M-A-Mohit-Khan <m.a.mohit.khan@gmail.com>
2023-01-17 07:10:54 +0600

    First commit & added array.cpp

---
+++ Essential Concepts/array.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int arr[5];                 //Declaring array with 5 int space
+    //Assigning value
+    arr[0]=1;
+    arr[1]=2;
+    arr[2]=3;
+    arr[3]=4;
+    arr[4]=5;
+    int arr1[5]={1,2,3,4,5};    //Declaring and initializing array
+    for(int i=0;i<5;i++)
+    {
+        cout<<arr1[i]<<endl;    //Printing values of arr1
+    }
+    return 0;
+}
